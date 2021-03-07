a416f68de406a90c1aeee6c2d82fc98507474e5e
learngirish <80162629+learngirish@users.noreply.github.com>
2021-03-07 19:32:18 +0530

    Create puppetagent.sh

---
+++ puppetagent.sh
@@ -0,0 +1,7 @@
+sudo su -
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+dpkg -i puppetlabs-release-pc1-xenial.deb
+apt-get update
+apt-get install puppet-agent
+systemctl start puppet
+systemctl status puppet
