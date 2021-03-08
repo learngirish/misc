cc8612859e6eacfb9c21c39d3c541b2c85697ef8
learngirish <80162629+learngirish@users.noreply.github.com>
2021-03-08 12:46:46 +0530

    Update dockermanif.pp

--- dockermanif.pp
+++ dockermanif.pp
@@ -1,23 +1,10 @@
-class docker { 
-       package {'curl': 
-               ensure  => present, 
-       } 
-       exec {'apt-update': 
-               command => '/usr/bin/apt-get update' 
-       } 
-       exec {'download_docker_key': 
-               command => '/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -' 
-       } 
-       exec {'add_docker_repo': 
-               command => '/usr/bin/add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"',  
-               require => Exec['apt-update'] 
-       } 
-       exec {'docker_cache': 
-               command => '/usr/bin/apt-cache policy docker-ce' 
-       } 
-       exec {'install_docker': 
-               command => '/usr/bin/apt-get install -y docker-ce' 
-       } 
-}
+class install_docker {
 
-class{'docker':}
+  # simple install with all the default options
+  include ::docker
+
+  # or you can customise the install
+  class { 'docker' :
+    manage_package => true,
+    package_name   => 'docker-engine',
+  }
